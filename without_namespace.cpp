333b5c27d7169ecf3acdbdf1d0b15b963d9eb3fc
Sourojit Banerjee <181811476+Sourojitbanerjee@users.noreply.github.com>
2025-06-27 16:14:06 +0530

    souro

---
+++ without_namespace.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"sourojit"<<std::endl;
+}
+//or 
+#include<iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"sourojit banerjee"<<endl;
+}
